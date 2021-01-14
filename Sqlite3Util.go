8aac94dc3a3f0b1fdd0bdca981ccaf293a02b026
wing <yuanxing2002@163.com>
2021-01-14 12:53:51 +0800

    go-util

--- Sqlite3Util.go
+++ Sqlite3Util.go
@@ -2,8 +2,8 @@ package util
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
 )
 
 type Sqlite3 struct {
@@ -14,7 +14,7 @@ func NewSqlite3Connection(DBFile string) (*Sqlite3, error) {
 	sl := &Sqlite3{}
 	db, err := sql.Open("sqlite3", DBFile)
 	if err != nil {
-		return nil, fmt.Errorf("Sqlite3 %s open error:%v ", DBFile, err)
+		return nil, errors.Errorf("Sqlite3 %s open error:%v ", DBFile, err)
 	}
 	sl.DbConn = db
 	return sl, nil
